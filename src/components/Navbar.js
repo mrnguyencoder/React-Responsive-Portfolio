ac79c569f2f95b8dd2301a348bd721caea2bcf90
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-11-28 11:20:59 +0100

    setup pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='toggleButton'>
+            <button></button>
+        </div>
+        <div className='links'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
