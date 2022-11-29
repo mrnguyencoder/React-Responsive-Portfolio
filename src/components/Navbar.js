862e9c6d7b3bcfd526e641635124047c0213bea7
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-11-29 22:01:17 +0100

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {Link, useLocation} from 'react-router-dom';
-import ReorderIcon from '@material-ui/icons/Reorder';
+import React, {} from 'react';
+import {Link} from 'react-router-dom';
 import "../styles/Navbar.css";
 
 function Navbar() {
-const [expandNavbar, setExpandNavbar] = useState(false);
-const location = useLocation();
-
-    useEffect(() => {
-    setExpandNavbar(false);
-    }, [location]);
-
   return (
-    <div className='navbar' id={expandNavbar ? "open" : "close" }>
-        <div className='toggleButton'>
-            <button onClick={() => {
-                setExpandNavbar((prev) => !prev);
-                }}
-            >
-                <ReorderIcon />
-            </button>
-        </div>
-        <div className='links'>
+    <>
+    <div className='navbar'>
+        <div className='logo'>nguyencoder</div>
+        <div className='links'>   
             <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
         </div>
     </div>
+    </>
   )
 }
 
